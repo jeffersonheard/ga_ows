07af1f17ea5d1a79631010cee9a77471f8dddd13
Jefferson Heard <jeff@terrahub.io>
2015-02-11 15:03:06 -0500

    synched migrations across production

--- migrations/0001_initial.py
+++ migrations/0001_initial.py
@@ -1,57 +1,20 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
 
-from django.db import models, migrations
-import django.contrib.gis.db.models.fields
 
+class Migration(SchemaMigration):
 
-class Migration(migrations.Migration):
+    def forwards(self, orm):
+        pass
 
-    dependencies = [
-    ]
+    def backwards(self, orm):
+        pass
 
-    operations = [
-        migrations.CreateModel(
-            name='OGRDataset',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('location', models.TextField()),
-                ('checksum', models.CharField(max_length=32)),
-                ('name', models.CharField(max_length=255, db_index=True)),
-                ('human_name', models.TextField(db_index=True, null=True, blank=True)),
-                ('extent', django.contrib.gis.db.models.fields.PolygonField(srid=4326)),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='OGRDatasetCollection',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=255)),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='OGRLayer',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=255, db_index=True)),
-                ('human_name', models.TextField(db_index=True, null=True, blank=True)),
-                ('extent', django.contrib.gis.db.models.fields.PolygonField(srid=4326)),
-                ('dataset', models.ForeignKey(to='ga_ows.OGRDataset')),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-        migrations.AddField(
-            model_name='ogrdataset',
-            name='collection',
-            field=models.ForeignKey(to='ga_ows.OGRDatasetCollection'),
-            preserve_default=True,
-        ),
-    ]
+    models = {
+        
+    }
+
+    complete_apps = ['ga_ows']
